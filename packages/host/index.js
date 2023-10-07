825dd3eb29cb5831de810c623d85155e773fc305
Sharaf <sharaf.innovazions@gmail.com>
2023-10-07 22:59:59 +0400

    first commit

---
+++ packages/host/index.js
@@ -0,0 +1,53 @@
+/**
+ * @format
+ */
+
+import {ScriptManager, Script, Federated} from '@callstack/repack/client';
+import {AppRegistry, Platform} from 'react-native';
+import App from './src/App';
+import getContainersURL from '../catalog-server/utils/getContainersURL';
+import {name as appName} from './app.json';
+import {version as appVersion} from './package.json';
+
+ScriptManager.shared.addResolver(async (scriptId, caller) => {
+  const containersURL = getContainersURL({
+    hostname: process.env.SAS_CATALOG_SERVER_URL,
+    version: appVersion,
+    platform: Platform.OS,
+    appName,
+  });
+
+  const containersResponse = await fetch(containersURL);
+
+  const containers = await containersResponse.json();
+
+  const resolveURL = Federated.createURLResolver({
+    containers,
+  });
+
+  let url;
+  if (__DEV__ && caller === 'main') {
+    url = Script.getDevServerURL(scriptId);
+  } else {
+    url = resolveURL(scriptId, caller);
+  }
+
+  console.log('Script ID', scriptId);
+  console.log('caller', caller);
+  console.log('Resolved URL', url);
+
+  if (!url) {
+    return undefined;
+  }
+
+  return {
+    url,
+    cache: !__DEV__,
+    query: {
+      platform: Platform.OS, // only needed in development
+    },
+    verifyScriptSignature: __DEV__ ? 'off' : 'strict',
+  };
+});
+
+AppRegistry.registerComponent(appName, () => App);
